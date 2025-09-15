129578d3b84a49a6c0c933955be5a035e66bf751
Luca Michaelis <Luca.Michaelis@thinfabrics.com>
2025-09-15 12:11:49 +0200

    Remade classes, api file and functions file in new framework

---
+++ Classes/Board.cs
@@ -0,0 +1,14 @@
+
+namespace LumakaStickerQuestBackend.Classes
+{
+	public class Board
+	{
+		/// <summary>
+		/// Class to store information about the bingo board
+		/// </summary>
+
+		public int Id { get; set; }
+		public Field[] Fields { get; set; }
+		public bool IsCompleted { get; set; }
+	}
+}
---
+++ Classes/ListItem.cs
@@ -0,0 +1,17 @@
+
+namespace LumakaStickerQuestBackend.Classes
+{
+	public class ListItem
+	{
+		/// <summary>
+		/// Class to store information of list items
+		/// </summary>
+
+		public int Id { get; set; }
+		public int Position { get; set; }
+		public string Description { get; set; }
+		public int Category {  get; set; }
+		public bool CheckBox { get; set; }
+		public int Points { get; set; }
+	}
+}
