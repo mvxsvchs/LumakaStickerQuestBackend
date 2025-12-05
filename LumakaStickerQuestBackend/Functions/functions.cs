50534089337b54a706270fd4f6cafb0f75518265
Luca Michaelis <Luca.Michaelis@thinfabrics.com>
2025-12-05 12:42:57 +0100

    Amend to DeleteUser, forgot to add value to query

--- LumakaStickerQuestBackend/Functions/functions.cs
+++ LumakaStickerQuestBackend/Functions/functions.cs
@@ -152,6 +152,7 @@ namespace LumakaStickerQuestBackend.Functions
 				try
 				{
 					await using var cmd = new NpgsqlCommand(sql, conn);
+					cmd.Parameters.AddWithValue("user_id", id);
 
 					int rowsAffected = await cmd.ExecuteNonQueryAsync();
 					return rowsAffected == 1; // i want to add validation here, but db connection currently fails
